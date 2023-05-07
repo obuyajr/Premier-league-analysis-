e3567377554aafbbe6aabc30b11e80670f95bf77
obuyajr <66009556+obuyajr@users.noreply.github.com>
2023-05-07 13:35:12 +0300

    Update App.py

--- App.py
+++ App.py
@@ -14,3 +14,19 @@ else:
 
     print('Error: Could not get data from the website')
 
+import pandas as pd
+
+data = pd.read_csv('premier_league_data.csv')
+
+# drop duplicates
+
+data = data.drop_duplicates()
+
+# drop rows with missing values
+
+data = data.dropna()
+
+# drop irrelevant columns
+
+data = data.drop(['column1', 'column2'], axis=1)
+
